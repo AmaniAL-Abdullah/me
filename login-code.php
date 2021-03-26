3af6fc6920b1adf0b59ab0def7a6eebca97ee996
AmaniAL-Abdullah <79908839+AmaniAL-Abdullah@users.noreply.github.com>
2021-03-26 20:51:33 +0300

    Create login-code.php

---
+++ login-code.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Dosed horror - Login</title>
+  </head>
+<body>
+
+<body style="direction:rtl; text-align:right; background-color:#bb2222;font-weight:bold;">
+
+<form action="login.php" method="post">
+📩 Email:    <input type="text" name="email">
+<br>
+🔐 Passw: <input type="password" name="pass">
+<br><br>
+<input type="submit" value="دخول 💀">
+</form>
+
+
+<center><a style="text-decoration: none;color:black;font-size:14px;" href="https://techcampus.com/bootcamp?piow" target="_blank"><br>
+👹- Dosed horror - 👹 </a></center>
+
+
+</body>
+</html>
