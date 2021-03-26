73bcdc02e7cedb49ac7bfcb36138728cce20dbae
AmaniAL-Abdullah <79908839+AmaniAL-Abdullah@users.noreply.github.com>
2021-03-26 17:07:42 +0300

    Create login-code.php
    Create a php login code for a horror potion site

---
+++ login-code.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dosed horror site</title>
+</head>
+<body style="direction:rtl; text-align:right; background-color:#bb2222;font-weight:bold;">
+
+
+
+<form action="print.php" method="post">
+  😈 الإسم <br>
+  <input type="text" name="name"><br>
+   🔐 الرقم السري <br>
+  <input type="password" name="password"><br>
+   📩 البريد <br>
+  <input type="email" name="email"><br>
+   📞 رقم الجوال <br>
+  <input type="text" name="mobile" maxlength="12"><br>
+   🤡 افضل دورة تحبها؟ <br>
+  <input type="text" name="PIOW" value="Dosed horror Special" readonly>
+
+<br><br>
+
+🧱 الجنس
+<br>
+  <input type="radio" name="gender" value="male" checked> ⚔️ذكر<br>
+  <input type="radio" name="gender" value="female"> 🔪أنثى<br>
+
+<br><br>
+
+<textarea name="message" rows="10" cols="30">
+Dosed horror Message
+</textarea>
+
+<br><br>
+☠️ أي 
+الخدمات تفضل؟
+<br>
+  <input type="checkbox" name="lang1" value="python"> استشارة❓🧨<br>
+  <input type="checkbox" name="lang2" value="php">تدريب💀⛏<br>
+
+<br><br>
+<button action="submit"> ❌التسجيل❌</button>
+</form>
+
+
+
+<center><a style="text-decoration: none;color:black;font-size:14px;"  target="_blank"><img src="ho.jpeg" width="50px"><br>
+👹- Dosed horror - 👹</a></center>
+
+
+</body>
+</html>
